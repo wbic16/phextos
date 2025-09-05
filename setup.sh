98856b88876fe960207b1af432bd9ea986f7afae
Will Bickford <wbic16@gmail.com>
2025-09-04 23:42:45 -0600

    Baseline environment setup from the 3.22.1 boot environment

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-apk add tmux openssh-server openssh-client git
+LEVEL=0
+PVF="/etc/phextos.version"
+if [ -f $PVF ]; then
+  LEVEL=`cat $PVF`
+fi
+if [ $LEVEL -lt 1 ]; then
+  apk add tmux openssh-server openssh-client git
+  LEVEL=1
+fi
+if [ $LEVEL -lt 2 ]; then
+  cp etc/network/interfaces /etc/network/
+  cp etc/apk/repositories /etc/apk/
+  rc-update add networking boot
+  service networking restart
+  LEVEL=2
+fi
+echo "PhextOS Ready"
+echo "Level: $LEVEL"
+echo -n $LEVEL >$PVF
